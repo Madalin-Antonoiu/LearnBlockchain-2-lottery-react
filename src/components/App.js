e073db218f4de4bb529be44821bab1736d438804
mantonoiu <antonoiumadalin@gmail.com>
2021-03-11 15:40:24 +0000

    Can read data from the blockchain! Retrieves manager address :)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import lottery from '../ethereum/lottery'
 
-const App = () => {
-    return <div>Hello</div>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { manager: '' };
+    }
+
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        this.setState({ manager });
+    }
+
+    render() {
+        return <div>
+            <h2>Lottery Smart Contract</h2>
+            <p>This contract is managed by: {this.state.manager} </p>
+        </div>
+    }
 }
 
 export default App;

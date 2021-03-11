5e43964877ef35e0730b7117821ef5c2bc3f081c
mantonoiu <antonoiumadalin@gmail.com>
2021-03-11 16:01:46 +0000

    Read Manager, Players and Balance data off the blockchain smart contract :)

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import lottery from '../ethereum/lottery'
+import web3 from '../web3';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { manager: '' };
-    }
+    state = { manager: '', players: [], balance: '' };
 
     async componentDidMount() {
         const manager = await lottery.methods.manager().call();
-        this.setState({ manager });
+        const players = await lottery.methods.getPlayers.call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+
+        this.setState({ manager, players, balance });
     }
 
     render() {
         return <div>
             <h2>Lottery Smart Contract</h2>
-            <p>This contract is managed by: {this.state.manager} </p>
+            <p>This contract is managed by: {this.state.manager}</p>
+            <p>There are currently [{this.state.players.length}] people entered.</p>
+            <p> They are competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ETH ! </p>
+
         </div>
     }
 }

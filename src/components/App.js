68e431c24121c20eac4eb38c31da43304c71952c
mantonoiu <antonoiumadalin@gmail.com>
2021-03-11 16:18:09 +0000

    Form Setup

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,17 @@ class App extends React.Component {
         this.setState({ manager, players, balance });
     }
 
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        })
+    }
+
     render() {
         return <div>
             <h2>Lottery Smart Contract</h2>
@@ -21,7 +32,7 @@ class App extends React.Component {
             <p> They are competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ETH ! </p>
             <hr />
 
-            <form >
+            <form onSubmit={this.onSubmit}>
                 <h4>Want to try your luck?</h4>
                 <div>
                     <label htmlFor="input">Ammount of ETH to enter</label>
